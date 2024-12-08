ea7b476062260226b64f7e1e0867119ca989df77
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-12-09 00:39:17 +0530

    redesign landing page - hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,94 @@
 import FlickeringGrid from "@/components/ui/flickering-grid";
 import { MagicCard } from "@/components/ui/magic-card";
+import { AnimatedList } from "@/components/ui/animated-list";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+
+interface Item {
+  name: string;
+  icon: string;
+  color: string;
+}
 
 export default function Home() {
   const theme = "dark";
+  let notifications = [
+    {
+      name: "Skill-Based Learning",
+      icon: "💬",
+      color: "#FFA800",
+    },
+    {
+      name: "Career-Ready Guidance",
+      icon: "👤",
+      color: "#FF6624",
+    },
+    {
+      name: "Tailored for You",
+      icon: "💬",
+      color: "#ff4d00",
+    },
+    {
+      name: "AI-Driven Roadmaps",
+      icon: "💸",
+      color: "#FF2E00",
+    },
+  ];
+   
+  notifications = Array.from({ length: 10 }, () => notifications).flat();
+  const Notification = ({ name, icon, color }: Item) => {
+    return (
+      <figure
+        className={cn(
+          "relative mx-auto min-h-fit w-full max-w-5/6 cursor-pointer overflow-hidden rounded-2xl px-4 py-2",
+          "transition-all duration-200 ease-in-out hover:scale-[103%]",
+          "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+          "transform-gpu dark:bg-neutral-950 dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        )}
+      >
+        <div className="flex flex-row items-center gap-3">
+          <div
+            className="flex size-10 items-center justify-center rounded-2xl"
+            style={{
+              backgroundColor: color,
+            }}
+          >
+            <span className="text-lg">{icon}</span>
+          </div>
+          <div className="flex flex-col overflow-hidden">
+            <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
+              <span className="text-sm md:text-md">{name}</span>
+              <span className="mx-1">·</span>
+            </figcaption>
+          </div>
+        </div>
+      </figure>
+    );
+  };
+
   return (
     <div className="min-h-screen text-white ">
       <div className="w-screen h-screen bg-netural-950">
-        <div className="h-1/2 mx-10 mt-28 rounded-xl bg-[#ff4d00] p-5">
-        <h2 className="w-2/6 text-5xl font-extrabold text-black">Master Coding with Personalized Learning Plans</h2>
-        <p className="md:w-1/2 font-mono mt-5">CodingAashram leverages AI to create personalized learning paths based on your current skills, guiding you to land your dream software development job.</p>
-        </div>
-        <div className="h-1/4 mx-10 my-5 rounded-xl flex gap-5">
-          <div className="h-full w-3/4 rounded-xl bg-neutral-100">
+        <div className="h-fit mx-10 mt-28 rounded-xl bg-[#ff4d00] py-7 px-5 flex">
+          <div>
+            <h2 className="w-full md:w-2/6 text-4xl md:text-5xl font-extrabold text-black">Master Coding with Personalized Learning Plans</h2>
+            <p className="md:w-1/2 font-mono mt-5">CodingAashram leverages AI to create personalized learning paths based on your current skills, guiding you to land your dream software development job.</p>
           </div>
-          <div className="h-full w-1/4 rounded-xl bg-neutral-500 p-5">
-            <ul className="">
-              <li className="text-white text-xl font-semibold text-right m-3">AI-Driven Roadmaps</li>
-              <li className="text-white text-xl font-semibold text-right m-3">Skill-Based Learning</li>
-              <li className="text-white text-xl font-semibold text-right m-3">Career-Ready Guidance</li>
-            </ul>
+          <div></div>
+        </div>
+        <div className="h-2/4 md:h-1/4 mx-10 my-5 rounded-xl flex flex-col md:flex-row gap-5">
+          <div className="h-1/2 md:h-full md:w-3/4 rounded-xl p-2 bg-neutral-100 text-xs md:text-lg font-bold text-black flex items-center justify-around gap-2">
+            <h2 className="border-2 px-2 py-4 md:py-3 md:px-5 rounded-md md:rounded-xl bg-[#ff4d00]">1.Create account</h2>
+            <h2 className="border-2 px-2 py-4 md:py-3 md:px-5 rounded-md md:rounded-xl bg-[#ff4d00]">2.Provide Details</h2>
+            <h2 className="border-2 px-2 py-4 md:py-3 md:px-5 rounded-md md:rounded-xl bg-[#ff4d00]">3.Generate Roadmap</h2>
+            <h2 className="border-2 px-2 py-4 md:py-3 md:px-5 rounded-md md:rounded-xl bg-[#ff4d00]">4.Follow It</h2>
+          </div>
+          <div className="h-2/3 md:h-full md:w-1/4 rounded-xl p-5 relative flex flex-col overflow-hidden border bg-neutral-600 md:shadow-xl">
+              <AnimatedList>
+                {notifications.map((item, idx) => (
+                  <Notification {...item} key={idx} />
+                ))}
+              </AnimatedList>
           </div>
         </div>
       </div>
